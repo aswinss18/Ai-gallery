d944fccded42e158e16a7d372fad024c6e8aa960
Aswin <aswinss0018@gmail.com>
2025-01-13 19:47:59 +0530

    clerk JS implemented.

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 export default function Home() {
-  return <div>home</div>;
+  return (
+    <div>
+      <p>Home</p>
+      {/* Removed afterSignOutUrl; the behavior is now configured via Clerk settings */}
+      <UserButton />
+    </div>
+  );
 }
