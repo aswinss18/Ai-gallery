37ad93d3eb1cbf9d4b89be187fb4bd00636320d6
Aswin <aswinss0018@gmail.com>
2025-01-15 16:24:14 +0530

    bug fix

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -5,8 +5,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
-import { navLinks } from "../../../constants";
-import { Button } from "../ui/button";
+import { navLinks } from "@constants/index";
+import { Button } from "@/components/ui/button";
 
 export default function Sidebar() {
   const pathname = usePathname();
