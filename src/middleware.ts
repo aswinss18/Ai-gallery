dfeb6140099e48ca962cfd0cef81608f90f00de9
Aswin <aswinss0018@gmail.com>
2025-01-16 21:56:47 +0530

    middleware updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,15 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Skip Next.js internals and static files
+    "/((?!_next|.*\\..*).*)", // Match all routes except static files
+
+    // Allow public routes
+    "/public-route", // Add your public routes here
+    // Add more public routes if needed
+
     // Always run for API routes
-    "/(api|trpc)(.*)",
+    "/api/(.*)",
+    "/trpc/(.*)",
   ],
 };
